f59a78c5f96cc2594adb272bc0d80cab971f12c0
Vitalii Bashkiser <vitalikbashkiser@gmail.com>
2023-10-02 21:48:47 +0300

    Added Django Models links, Provided Migrations Resources, Referenced DRF Serializers, Initialized Model Migrations, Verified Table Creation, Documented Migration Steps, Integrated Logging.

--- Backend/start.sh
+++ Backend/start.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# Wait for the database to become available
-/wait-for-it.sh db:5432 -- python manage.py migrate
+# Apply migrations
+python manage.py migrate
 
 # Start the server
 python manage.py runserver 0.0.0.0:8000
-
