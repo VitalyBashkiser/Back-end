7ae1b80d0e37b06ac18bf8d329e1ae53baf2596a
Vitalii Bashkiser <vitalikbashkiser@gmail.com>
2023-10-03 17:24:23 +0300

    Fix with a Django Rest Framework Serializers troubles

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'django_extensions'
+    'django_extensions',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
