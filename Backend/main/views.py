ed817efd147bd97a3880a4cbf1bf1a6bc83bfed3
Vitalii Bashkiser <vitalikbashkiser@gmail.com>
2023-10-03 17:29:26 +0300

    Added UserSerializer, TimeStampedModel and SuperUser

--- Backend/main/views.py
+++ Backend/main/views.py
@@ -1,4 +1,11 @@
+from rest_framework import viewsets
+from .models import User
+from .serializers import UserSerializer
 from django.http import JsonResponse
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 def health_check(request):
     return JsonResponse({
