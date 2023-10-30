51d55c7636f9ccf3075d4c2be7f86b4c712dd1f7
Vitalii <87275923+VitalyBashkiser@users.noreply.github.com>
2023-10-30 09:48:00 -0700

    Update tests.py

--- Backend/quizzes/tests.py
+++ Backend/quizzes/tests.py
@@ -62,26 +62,26 @@ class QuestionTests2(APITestCase):
 
 
 class QuestionTests3(APITestCase):
-        def test_create_question_with_selected_answers(self):
-            quiz = Quiz.objects.create(title='Test Quiz', description='Description', frequency=7)
+    def test_create_question_with_selected_answers(self):
+        quiz = Quiz.objects.create(title='Test Quiz', description='Description', frequency=7)
 
-            url = reverse('create_question_with_selected_answers')
-            data = {
-                "quiz": quiz.id,
-                "question_text": "What is the capital of Japan?",
-                "answers": [
-                    {"answer_text": "Tokyo", "is_correct": False},
-                    {"answer_text": "Beijing", "is_correct": True},
-                    {"answer_text": "Seoul", "is_correct": False}
-                ]
-            }
-            response = self.client.post(url, data, format='json')
-            print(response.data)
-            self.assertEqual(response.status_code, 201)
-            question = Question.objects.get(id=response.data['id'])
-            self.assertEqual(question.question_text, "What is the capital of Japan?")
-            self.assertEqual(question.answers.count(), 3)
-            self.assertTrue(question.answers.filter(answer_text="Beijing", is_correct=True).exists())
+        url = reverse('create_question_with_selected_answers')
+        data = {
+            "quiz": quiz.id,
+            "question_text": "What is the capital of Japan?",
+            "answers": [
+                {"answer_text": "Tokyo", "is_correct": False},
+                {"answer_text": "Beijing", "is_correct": True},
+                {"answer_text": "Seoul", "is_correct": False}
+            ]
+        }
+        response = self.client.post(url, data, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, 201)
+        question = Question.objects.get(id=response.data['id'])
+        self.assertEqual(question.question_text, "What is the capital of Japan?")
+        self.assertEqual(question.answers.count(), 3)
+        self.assertTrue(question.answers.filter(answer_text="Beijing", is_correct=True).exists())
 
 
 class QuizTests(TestCase):
